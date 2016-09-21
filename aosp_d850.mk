08b1a55c1116d09540cb56d3f714b3d6f84f92d4
Richard Covington <rcovington79@gmail.com>
2016-09-20 21:43:34 -0400

    Add files via upload

--- aosp_d850.mk
+++ aosp_d850.mk
@@ -17,9 +17,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common AOSP stuff.
-$(call inherit-product, vendor/aosp/common.mk)
-
 # Inherit from d850 device
 $(call inherit-product, device/lge/d850/device.mk)
 
